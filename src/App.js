52ca08be95aa37ba331dbad5591aa074f356a21c
Mirjana Vukojevic <mirjana.vukojevic@quantox.com>
2018-12-21 12:23:25 +0100

    lists, key, section4

--- src/App.js
+++ src/App.js
@@ -5,20 +5,59 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {name: 'Stojko', age: 28},
-      {name: 'Mira', age: 38},
-      {name: 'Jebemga', age: 25}
-    ]
+      {id: 1, name: 'Stojko', age: 28},
+      {id: 2, name: 'Mira', age: 38},
+      {id: 3, name: 'Jebemga', age: 25}
+    ],
+    showPersons: false
+  }
+
+  deletePerosnHandler  = (event) => {
+    console.log(event)
+    const newArrayOfPersons = this.state.persons;
+    newArrayOfPersons.splice(event, 1);
+    this.setState({
+      persons: newArrayOfPersons
+    })
+  }
+   
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {...this.state.persons[personIndex]}
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState ({
+      persons: persons
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <button>Switch name</button>
-        
-        <Person age={this.state.persons[0].age} name={this.state.persons[0].name}/>
-        <Person age={this.state.persons[1].age} name={this.state.persons[1].name}/>
-        <Person age={this.state.persons[2].age} name={this.state.persons[2].name}/>
+      {
+        this.state.persons.map((person, index) => {
+          return <Person key={index} klik={() => this.deletePerosnHandler(index)} name={person.name} age={person.age} change={(event) => this.nameChangedHandler(event, person.id)}/>
+        })
+      }
+        <button onClick={this.togglePersonsHandler}>Switch name</button>
+        {!this.state.showPersons ? <div >
+          {/* <Person age={this.state.persons[0].age} name={this.state.persons[0].name} klik={this.switchNameHandler} change={this.nameChangedHandler}/>
+          <Person age={this.state.persons[1].age} name={this.state.persons[1].name}/>
+          <Person age={this.state.persons[2].age} name={this.state.persons[2].name}/> */}
+        </div> : null}
       </div>
     );
   
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
  import React from 'react';
  
  const person = (props) => {
-    return <p> I am {props.name} and i am {props.age} years old</p>
+    return (
+        <div>
+            <p onClick={props.klik}> I am {props.name} and i am {props.age} years old</p>
+            <input type="text" onChange={props.change}/>
+        </div>
+    )
  }
 
 export default person
